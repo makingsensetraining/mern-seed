4cac96f6390695b21f07dd647eabb624e7875f13
Juan Pablo Cook <juampick@gmail.com>
2017-01-18 17:34:26 -0300

    Make npm script run test all over the app folder with .test files. First brieft test on Redux store

---
+++ app/store/store.test.js
@@ -0,0 +1,44 @@
+import expect from 'expect';
+import {createStore} from 'redux';
+import rootReducer from '../reducers';
+import initialState from '../reducers/initialState';
+import * as userActions from '../actions/userActions';
+
+describe('Store', () => {
+    it('Should handle loading users', () => {
+        //arrange
+        const store = createStore(rootReducer, initialState.users);
+
+        const users = [
+            {
+                id: 1,
+                name: 'Test User 1',
+                email: 'testuser@makingsense.com',
+                createdAt: '2017-01-17'
+            },
+            {
+                id: 2,
+                name: 'Test User 2',
+                email: 'testuser2@makingsense.com',
+                createdAt: '2017-01-17'
+            },
+            {
+                id: 3,
+                name: 'Test User 3',
+                email: 'testuser3@makingsense.com',
+                createdAt: '2017-01-17'
+            }
+        ];
+
+        //act
+        const action = userActions.loadUserSuccess(users);
+        store.dispatch(action);
+
+        //assert
+        const actual = store.getState().userData.users;
+
+        // let expected = users;
+
+        expect(actual).toEqual(users);
+    });
+});
