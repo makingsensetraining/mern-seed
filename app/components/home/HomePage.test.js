07904e6227168e8bf39bbd7d8dd48f34d71fa060
Juan Pablo Cook <juampick@gmail.com>
2017-01-18 18:34:18 -0300

    Added test for HomePage React component.

---
+++ app/components/home/HomePage.test.js
@@ -0,0 +1,17 @@
+import expect from 'expect';
+import React from 'react';
+import {mount, shallow} from 'enzyme';
+import HomePage from './HomePage';
+
+function setup(){
+    return shallow(<HomePage />);
+}
+
+describe('HomePage', () => {
+    it('renders homepage, h1 and subtitle', () => {
+        const wrapper = setup(false);
+        expect(wrapper.find('div').length).toBe(1);
+        expect(wrapper.find('h1').text()).toEqual('MERN seed App');
+        expect(wrapper.find('p').text()).toEqual('MongoDB, ExpressJS, React, Node using Redux in ES6 for ultra-responsive webapps.');
+    });
+});
