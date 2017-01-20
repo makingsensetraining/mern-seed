d1f45f86947941df56cfaa520dc0819a263883a7
Juan Pablo Cook <juampick@gmail.com>
2017-01-20 12:33:45 -0300

    Fixed some param on stateless homepage component test and created test cases for userReducer.

--- app/components/home/HomePage.test.js
+++ app/components/home/HomePage.test.js
@@ -1,5 +1,5 @@
-import expect from 'expect';
 import React from 'react';
+import expect from 'expect';
 import {mount, shallow} from 'enzyme';
 import HomePage from './HomePage';
 
@@ -9,7 +9,7 @@ function setup(){
 
 describe('HomePage', () => {
     it('renders homepage, h1 and subtitle', () => {
-        const wrapper = setup(false);
+        const wrapper = setup();
         expect(wrapper.find('div').length).toBe(1);
         expect(wrapper.find('h1').text()).toEqual('MERN seed App');
         expect(wrapper.find('p').text()).toEqual('MongoDB, ExpressJS, React, Node using Redux in ES6 for ultra-responsive webapps.');
---
+++ app/reducers/userReducer.test.js
@@ -0,0 +1,65 @@
+import expect from 'expect';
+import {userData, user} from './userReducer';
+import * as actions from '../actions/userActions';
+
+describe('User Reducer', () => {
+    it('should load the users when passed LOAD_USER_SUCCESS', () => {
+
+        const initialStateUserData = []; //empty array
+
+        const usersParam = [
+            {
+                id: 1,
+                name: 'Test User 1',
+                email: 'testuser1@makingsense.com',
+                createdAt: '2017-01-20'
+            },
+            {
+                id: 2,
+                name: 'Test User 2',
+                email: 'testuser2@makingsense.com',
+                createdAt: '2017-01-20'
+            }
+        ];
+
+        const action = actions.loadUserSuccess(usersParam);
+
+        //act
+        const newState = userData(initialStateUserData, action);
+
+        //assert
+        expect(newState.users.length).toEqual(2);
+
+        expect(newState.users[0].id).toEqual(1);
+        expect(newState.users[0].name).toEqual('Test User 1');
+        expect(newState.users[0].email).toEqual('testuser1@makingsense.com');
+        expect(newState.users[0].createdAt).toEqual('2017-01-20');
+
+        expect(newState.users[1].id).toEqual(2);
+        expect(newState.users[1].name).toEqual('Test User 2');
+        expect(newState.users[1].email).toEqual('testuser2@makingsense.com');
+        expect(newState.users[1].createdAt).toEqual('2017-01-20');
+    });
+
+    it('should get the user when passed GET_USER_SUCCESS', () => {
+        const initialStateUserObject = {}; //empty object
+
+        const userParam = {
+            id: 1,
+            name: 'Test User 1',
+            email: 'testuser1@makingsense.com',
+            createdAt: '2017-01-20'
+        };
+
+        const action = actions.getUserSuccess(userParam);
+
+        //act
+        const newState = user(initialStateUserObject, action);
+
+        //assert
+        expect(newState.id).toEqual(1);
+        expect(newState.name).toEqual('Test User 1');
+        expect(newState.email).toEqual('testuser1@makingsense.com');
+        expect(newState.createdAt).toEqual('2017-01-20');
+    });
+});
